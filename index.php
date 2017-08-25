b0e35d1db2683c41acb669fc10aec4db83c30442
selma <selma@gmail.com>
2017-08-25 11:04:08 +0200

    include partial

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 
 require 'vendor/autoload.php';
 
-$m = new Mustache_Engine();
-echo $m->render('Hello {{name}}!', [
+$m = new Mustache_Engine([
+     'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/template', [
+         'extension' => '.html'])
+]);
+
+// render c'est pour afficher le contenu de html
+echo $m->render('index', [
 'name' => 'World'
 ]);
